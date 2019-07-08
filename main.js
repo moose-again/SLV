aad0b481f4cdf1ceb33dbec201ef034b3464b21d
moose1645 <43937916+moose1645@users.noreply.github.com>
2019-07-08 13:53:35 -0500

    more logo data

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const logoData = [
-    {
+
+/*Template:
+
         name:"",
         group:"",
         sport:"",
@@ -7,7 +9,38 @@ const logoData = [
         vectorFile: "",
         png500: "",
         png1000: "",
+        colors: [],
+        colorsUsed: [],
+        year: "",
+
+*/
         
+    //NFL
+    {
+        name:"Arizona Cardinals",
+        group:"NFL",
+        sport:"Football",
+        tags: ["birds", "red", "animals"],
+        vectorFile: "logos/NFL/vector/ARI.svg",
+        png500: "logos/NFL/png/500/ARI.png",
+        png1000: "logos/NFL/png/1000/ARI.png",
+        colors: ["#97233f", "#ffffff"],
+        colorsUsed: ["#97233f", "#ffb612", "#000000", "#ffffff"],
+        year: "2005",
+        
+        
+    },
+    {
+        name:"Atlanta Falcons",
+        group:"NFL",
+        sport:"Football",
+        tags: ["birds", "black", "animals"],
+        vectorFile: "logos/NFL/vector/ATL.svg",
+        png500: "logos/NFL/png/500/ATL.png",
+        png1000: "logos/NFL/png/1000/ATL.png",
+        colors: ["#a71930", "#ffffff", "#a5acaf", "#000000"],
+        colorsUsed: ["#a71930", "#ffffff", "#a5acaf", "#000000"],
+        year: "2003",
         
     }
 ]
