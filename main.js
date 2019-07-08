214c04e20f31fe36c0a46d6d87a5705128333540
moose1645 <43937916+moose1645@users.noreply.github.com>
2019-07-08 14:28:01 -0500

    More data

--- main.js
+++ main.js
@@ -42,5 +42,57 @@ const logoData = [
         colorsUsed: ["#a71930", "#ffffff", "#a5acaf", "#000000"],
         year: "2003",
         
-    }
+    },
+    {
+        name:"Baltimore Ravens",
+        group:"NFL",
+        sport:"Football",
+        tags: ["birds", "purple", "animals"],
+        vectorFile: "logos/NFL/vector/BAL.svg",
+        png500: "logos/NFL/png/500/BAL.png",
+        png1000: "logos/NFL/png/1000/BAL.png",
+        colors: ["#241773", "#9e7c0c", "#000000"],
+        colorsUsed: ["#241773", "#9e7c0c", "#000000", "#ffffff", "#c60c30"],
+        year: "1999",
+        
+    },
+    {
+        name:"Buffalo Bills",
+        group:"NFL",
+        sport:"Football",
+        tags: ["blue", "animals"],
+        vectorFile: "logos/NFL/vector/BUF.svg",
+        png500: "logos/NFL/png/500/BUF.png",
+        png1000: "logos/NFL/png/1000/BUF.png",
+        colors: ["#00338d", "#c60c30", "#ffffff", "#00274d"],
+        colorsUsed: ["#00338d", "#c60c30", "#ffffff"],
+        year: "1974",
+        
+    },
+    {
+        name:"Carolina Panthers",
+        group:"NFL",
+        sport:"Football",
+        tags: ["cats","blue", "animals"],
+        vectorFile: "logos/NFL/vector/CAR.svg",
+        png500: "logos/NFL/png/500/CAR.png",
+        png1000: "logos/NFL/png/1000/CAR.png",
+        colors: ["#000000", "#0085ca", "#bfc0bf"],
+        colorsUsed: ["#000000", "#0085ca", "#bfc0bf"],
+        year: "2012",
+        
+    },
+    {
+        name:"Chicago Bears",
+        group:"NFL",
+        sport:"Football",
+        tags: ["letters","orange"],
+        vectorFile: "logos/NFL/vector/CHI.svg",
+        png500: "logos/NFL/png/500/CHI.png",
+        png1000: "logos/NFL/png/1000/CHI.png",
+        colors: ["#c83804", "#0b162a", "#ffffff"],
+        colorsUsed: ["#c83804", "#0b162a", "#ffffff"],
+        year: "1974",
+        
+    },
 ]
